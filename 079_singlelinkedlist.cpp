67f39132f976b6b239a138d2f7fd829603d63bf4
asyiraafnufail <asyiraaf4@gmail.com>
2024-04-26 14:46:04 +0700

    menuliskan while pilihan

--- 079_singlelinkedlist.cpp
+++ 079_singlelinkedlist.cpp
@@ -179,3 +179,10 @@ int main() {
                 cout << "Pilihan tidak ada" << endl;
                 break;
             }
+        }
+        catch (exception e)
+        {
+            cout << "Terjadi kesalahan" << endl;
+        }
+    } while (pilihan != 5);
+}
